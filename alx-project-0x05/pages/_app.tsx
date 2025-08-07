32008b957c5dfc2616e3f78f1b72085166d1590f
Afubwa. <emmanelafubwa@gmail.com>
2025-08-07 18:05:10 +0300

    Fix typo in App component props and adjust formatting in _app.tsx

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,14 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CountProvider } from "@/context/CountContext";
+import Layout from "@/components/layouts/Layout";
 
-export default function App ({Component, pagesProps}: AppProps) {
+export default function App ({Component, pageProps}: AppProps) {
   return (
     <CountProvider>
       {/* Wrap the application with CountProvider to provide count context */}
-    <Layout>
-      <Component {...pagesProps} />
-    </Layout>  
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>  
     </CountProvider>
   )
 }
