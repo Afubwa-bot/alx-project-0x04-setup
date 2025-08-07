1d18a71ebec2fa56c278eef4e93bb4f9107881a0
Afubwa. <emmanelafubwa@gmail.com>
2025-08-07 15:12:54 +0300

    Adding a counter app to our platform with useState Hook

---
+++ alx-project-0x04/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "@/components/layouts/Layout";
+
+ function App({ Component, pageProps }: AppProps) {
+  return 
+  ( <Layout>
+  <Component {...pageProps} />
+  </Layout>
+  );  
+}
+ export default App;
+// This file is the main entry point for the Next.js application.
